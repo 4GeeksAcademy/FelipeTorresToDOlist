8cfbe1614317bbb7740528d6f88bf488e303b96d
SrTorres07 <srtorresfelipe07@outlook.com>
2025-01-30 19:47:19 +0000

    envio trabajo al final tiene un mensaje

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
-import React from "react";
+import React,{useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
+
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [inputValue,setInputValue]= useState("")
+	const [todos,setTodos]= useState([])
+	
+	const DeleteAllTasks = ()=>{
+		setTodos([])
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+			<h1>ToDo LiSt</h1>
+			<ul>
+				<li><input type="text"
+				 	onChange={(e)=> setInputValue(e.target.value)}
+					value={inputValue} placeholder="Add Tasks"
+					onKeyUp={(e)=>{
+					if(e.key === "Enter" && inputValue.trim()=== "") {
+						return alert( "add tasks lazy");
+							}
+							else if(e.key === "Enter"){
+								setTodos(todos.concat(inputValue));
+								setInputValue(""); 
+									}
+						}}
+						></input>
+				</li>
+				{todos.map((item,index) => (
+					<li>
+						<FontAwesomeIcon 
+						onClick={()=> setTodos(todos.filter((t,currentIndex)=> index != currentIndex))}className="icono" icon={faTrash}/>
+						{item}{" "}
+					</li>
+				))}
+			</ul>
+			<div className="totaltasks"> {todos.length}  All Tasks</div>
+			<button className="DeleteAllTasks" onClick={DeleteAllTasks}> Delete All Tasks </button>
+
+		</div>	
 	);
 };
 
 export default Home;
+
+
+
+
+/*me ayude con le video de guia, y desarrolle el resto de componentes, como el boton de eliminar
+ todas la tareas, el alert, el que al dar enter no devolviera nada porque devolvia (,) y los estilos, les di un toque diferente, aun me falta mucha logica, pero le voy dando */ 
